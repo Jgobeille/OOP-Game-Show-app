38775f5f334acfd3a3a22447f01330146f4f92d8
Jamie Gobeille <djent34@gmail.com>
2020-01-29 11:23:36 +0900

    enabled feature to block user from opening dev tools to see the answer

--- js/app.js
+++ js/app.js
@@ -52,20 +52,20 @@ window.addEventListener("keydown", e => {
 //Disable the ability to view source code
 // https://stackoverflow.com/questions/24319786/how-to-hide-form-code-from-view-code-inspect-element-browser/24319844
 
-// document.onkeydown = function(e) {
-//   if (event.keyCode == 123) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-//     return false;
-//   }
-// };
+document.onkeydown = function(e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
